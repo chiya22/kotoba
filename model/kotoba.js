02cd6bd2f87a7449ae06b1d41b2885796f794c07
tomoyay <tomoya.yoshida@systemaid.co.jp>
2019-03-29 14:29:12 +0900

    modelに機能を委譲

--- model/kotoba.js
+++ model/kotoba.js
@@ -3,16 +3,16 @@ const config = require("../config/dbconfig.js");
 const { Client } = require("pg");
 const MAX_ITEMS_PER_PAGE = 2;
 
+// ■ findAll
 const findAll = ((query_page, query_value, callback) => {
   const client = new Client(config);
   client.connect();
 
-  let query_list = {};
-  let query_count = {};
+  let query_list = {}; //一覧取得用のquery
+  let query_count = {}; //総件数取得用のquery
   let page = query_page ? parseInt(query_page) : 1;
   let offset = (page - 1) * MAX_ITEMS_PER_PAGE;
   let kotobas = [];
-
   if (query_value) {
     query_list.text = "select * from kotoba where kotoba_value like '%' || $1 || '%'  order by kotoba_no limit $2 offset $3";
     query_list.values = [query_value, MAX_ITEMS_PER_PAGE, offset];
@@ -24,7 +24,6 @@ const findAll = ((query_page, query_value, callback) => {
     query_count.text = "select count(*) from kotoba";
     query_count.values = [];
   }
-
   //一覧の取得
   client.query(query_list, (err, result) => {
     if (err) {
@@ -34,6 +33,7 @@ const findAll = ((query_page, query_value, callback) => {
       kotobas = result.rows;
     }
   });
+  //総件数の取得
   client.query(query_count, (err, result) => {
     if (err) {
       callback(err, null);
@@ -55,9 +55,110 @@ const findAll = ((query_page, query_value, callback) => {
   });
 });
 
+// ■ find
+const find = ((kotoba_no, callback) => {
+  let query = {};
+  if (kotoba_no) {
+    query.text = "select * from kotoba where kotoba_no = $1";
+    query.values = [kotoba_no];
+  } else {
+    callback(new Error("invalid no"), null);
+  }
+  // connect
+  const client = new Client(config);
+  client.connect();
+  // 1件取得
+  client.query(query, (err, result) => {
+    if (err) {
+      callback(err, null);
+    }
+    client.end()
+      .catch((err) => {
+        throw err;
+      });
+    callback(null, result.rows[0]);
+  });
+});
+
+// ■ create
+const create = ((kotoba_value, callback) => {
+  let query = {};
+  if (kotoba_value) {
+    query.text = "insert into kotoba(kotoba_value) values ($1)";
+    query.values = [kotoba_value];
+  } else {
+    callback(new Error("kotoba_value is invalid"), null);
+  }
+  // connect
+  const client = new Client(config);
+  client.connect();
+  // INSERT
+  client.query(query, (err, result) => {
+    if (err) {
+      callback(err, null);
+    }
+    client.end()
+      .catch((err) => {
+        throw err;
+      });
+    callback(null, result.rows[0]);
+  });
+});
+
+// ■ update
+const update = ((kotoba_no, kotoba_value, callback) => {
+  let query = {};
+  if ((kotoba_value) && (kotoba_no)) {
+    query.text = "update kotoba set kotoba_value = $1 where kotoba_no = $2";
+    query.values = [kotoba_value, kotoba_no];
+  } else {
+    callback(new Error("kotoba_no or kotoba_value is invalid"), null);
+  }
+  // connect
+  const client = new Client(config);
+  client.connect();
+  // UPDATE
+  client.query(query, (err, result) => {
+    if (err) {
+      callback(err, null);
+    }
+    client.end()
+      .catch((err) => {
+        throw err;
+      });
+    callback(null, result.rows[0]);
+  });
+});
+
+// ■ remove
+const remove = ((kotoba_no, callback) => {
+  let query = {};
+  if (kotoba_no) {
+    query.text = "delete from kotoba where kotoba_no = $1";
+    query.values = [kotoba_no];
+  } else {
+    callback(new Error("kotoba_no is invalid"), null);
+  }
+  // connect
+  const client = new Client(config);
+  client.connect();
+  // DELETE
+  client.query(query, (err, result) => {
+    if (err) {
+      callback(err, null);
+    }
+    client.end()
+      .catch((err) => {
+        throw err;
+      });
+    callback(null, result.rows[0]);
+  });
+});
+
 module.exports = {
   findAll: findAll,
-  create: "",
-  update: "",
-  delete: ""
+  find: find,
+  create: create,
+  update: update,
+  delete: remove
 };
--- routes/admin.js
+++ routes/admin.js
@@ -19,12 +19,11 @@ router.get("/", (req, res) => {
     // token削除
     delete req.session._csrf;
     res.clearCookie("_csrf");
-    
     res.render("admin/list", retObj);
   });
 });
 
-//一覧→＜登録ボタン＞→実行画面
+//一覧より登録画面を開く
 //
 //page：一覧での表示していたページ数
 //q：一覧での検索文字列
@@ -34,7 +33,7 @@ router.get("/kotoba/touroku", (req, res) => {
     req.session._csrf = secret;
     res.cookie("_csrf", token);
     res.render("admin/kotoba_form", {
-      mode: "insert",
+      mode: "create",
       q: req.query.q,
       page: req.query.page,
       kotoba: {}
@@ -42,7 +41,7 @@ router.get("/kotoba/touroku", (req, res) => {
   });
 });
 
-//一覧→＜更新ボタンクリック＞→実行画面
+//一覧より更新画面を開く
 //
 //no：更新対象のkotoba_no
 //page：一覧での表示していたページ数
@@ -50,47 +49,32 @@ router.get("/kotoba/touroku", (req, res) => {
 router.get("/kotoba/koushin", (req, res) => {
   const client = new Client(config);
   client.connect();
-  let query = {};
   if (req.query.no) {
-    query.text = "select * from kotoba where kotoba_no = $1";
-    query.values = [req.query.no];
-    client.query(query, (err, result) => {
+    kotoba.find(req.query.no, (err, retObj) => {
       if (err) {
         throw err;
       }
-      client.end()
-        .catch((err) => {
-          throw err;
+      tokens.secret((error, secret) => {
+        if (error) {
+          throw Error("create token error");
+        }
+        const token = tokens.create(secret);
+        req.session._csrf = secret;
+        res.cookie("_csrf", token);
+        res.render("admin/kotoba_form", {
+          mode: "update",
+          q: req.query.q,
+          page: req.query.page,
+          kotoba: retObj
         });
-      if (result.rowCount === 0) {
-        res.redirect("/");
-      } else {
-        tokens.secret((error, secret) => {
-          if (error) {
-            throw Error("create token error");
-          }
-          const token = tokens.create(secret);
-          req.session._csrf = secret;
-          res.cookie("_csrf", token);
-          res.render("admin/kotoba_form", {
-            mode: "update",
-            q: req.query.q,
-            page: req.query.page,
-            kotoba: result.rows[0]
-          });
-        });
-      }
+      });
     });
   } else {
-    client.end()
-      .catch((err) => {
-        throw err;
-      });
     res.redirect("/");
   }
 });
 
-//一覧→＜削除ボタンクリック＞→実行画面
+//一覧より削除画面を開く
 //
 //no：削除対象のkotoba_no
 //page：一覧での表示していたページ数
@@ -98,81 +82,83 @@ router.get("/kotoba/koushin", (req, res) => {
 router.get("/kotoba/sakujyo", (req, res) => {
   const client = new Client(config);
   client.connect();
-  let query = {};
   if (req.query.no) {
-    query.text = "select * from kotoba where kotoba_no = $1";
-    query.values = [req.query.no];
-    client.query(query, (err, result) => {
+    kotoba.find(req.query.no, (err, retObj) => {
       if (err) {
         throw err;
       }
-      client.end()
-        .catch((err) => {
-          throw err;
+      tokens.secret((error, secret) => {
+        if (error) {
+          throw Error("create token error");
+        }
+        const token = tokens.create(secret);
+        req.session._csrf = secret;
+        res.cookie("_csrf", token);
+        res.render("admin/kotoba_form", {
+          mode: "delete",
+          q: req.query.q,
+          page: req.query.page,
+          kotoba: retObj
         });
-      if (result.rowCount === 0) {
-        res.redirect("/");
-      } else {
-        tokens.secret((error, secret) => {
-          const token = tokens.create(secret);
-          req.session._csrf = secret;
-          res.cookie("_csrf", token);
-          res.render("admin/kotoba_form", {
-            mode: "delete",
-            q: req.query.q,
-            page: req.query.page,
-            kotoba: result.rows[0]
-          });
-        });
-      }
+      });
     });
   } else {
-    client.end()
-      .catch((err) => {
-        throw err;
-      });
     res.redirect("/");
   }
 });
 
-//完了
-router.post("/kotoba/kanryou", (req, res) => {
-
+// 登録
+router.post("/kotoba/create", (req, res) => {
   const secret = req.session._csrf;
   const token = req.cookies._csrf;
   if (!tokens.verify(secret, token)) {
     throw Error("invalid token");
   }
-
-  let query = {};
-  if (req.body.mode === "insert") {
-    query.text = "insert into kotoba(kotoba_value) values ($1)";
-    query.values = [req.body.kotoba_value];
-  } else if (req.body.mode === "update") {
-    query.text = "update kotoba set kotoba_value = $1 where kotoba_no = $2";
-    query.values = [req.body.kotoba_value, req.body.kotoba_no];
-  } else if (req.body.mode === "delete") {
-    query.text = "delete from kotoba where kotoba_no = $1";
-    query.values = [req.body.kotoba_no];
-  }
-
-  const client = new Client(config);
-  client.connect();
-  client.query(query, (err, result) => {
-    if (err) {
+  kotoba.create(req.body.kotoba_value, (err, retObj) => {
+    if (err){
       throw err;
     }
-    client.end()
-      .catch((err) => {
-        throw err;
-      });
     delete req.session._csrf;
     res.clearCookie("_csrf");
-
     res.redirect("/admin/kotoba/kanryou");
   });
 });
 
+// 更新
+router.post("/kotoba/update",(req,res) => {
+  const secret = req.session._csrf;
+  const token = req.cookies._csrf;
+  if (!tokens.verify(secret, token)) {
+    throw Error("invalid token");
+  }
+  kotoba.update(req.body.kotoba_no, req.body.kotoba_value, (err, retObj) => {
+    if (err){
+      throw err;
+    }
+    delete req.session._csrf;
+    res.clearCookie("_csrf");
+    res.redirect("/admin/kotoba/kanryou");
+  });
+});
+
+// 削除
+router.post("/kotoba/delete", (req,res) => {
+  const secret = req.session._csrf;
+  const token = req.cookies._csrf;
+  if (!tokens.verify(secret, token)) {
+    throw Error("invalid token");
+  }
+  kotoba.delete(req.body.kotoba_no, (err, retObj) => {
+    if (err){
+      throw err;
+    }
+    delete req.session._csrf;
+    res.clearCookie("_csrf");
+    res.redirect("/admin/kotoba/kanryou");
+  });
+});
+
+// 完了
 router.get("/kotoba/kanryou", (req, res) => {
   res.render("admin/kotoba_complete", {});
 });
