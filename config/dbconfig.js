9dec313e225a679b0a8414b8340216d71fb8c921
tomoyay <tomoyay@gmail.com>
2019-04-23 05:59:03 +0900

    heroku用の設定

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,25 @@
 module.exports = {
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "oosaka@06",
+  user: "lpanmwkyzzgesd",
+  host: "ec2-54-83-205-27.compute-1.amazonaws.com",
+  database: "dadgqc3tiomiie",
+  password: "5299f4aee8d54269a134efa6d0e46e34574df40612db2eef029600e36b0e6c7d@06",
   port: 5432
 };
+// create sequence kotoba_no_seq
+//     increment by 1
+//     maxvalue 99999999
+//     start with 1
+//     cycle
+// ;
+// create table users (
+// name varchar(100) not null,
+// password varchar(100) not null,
+// role varchar(100),
+// primary key(name)
+// );
+// create table kotoba (
+// kotoba_no int default nextval('kotoba_no_seq'),
+// kotoba_value varchar(100) not null,
+// primary key(kotoba_no)
+// );
+
