fe8c92f2651114dc46c21aa216010ba5ae190b5c
tomoyay <tomoyay@gmail.com>
2019-05-26 09:48:19 +0900

    javascriptでダブルクリック時にアラートを表示し、再描画する

---
+++ public/javascripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+{
+  const container = document.querySelector(".container");
+  container.addEventListener("dblclick", () => {
+    alert("ダブルクリックしたよ");
+    document.location = "/";
+  });
+}
