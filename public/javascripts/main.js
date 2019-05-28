863ef3e656f2a63a56a252c50f30479493fa990c
tomoyay <tomoyay@gmail.com>
2019-05-28 10:22:54 +0900

    ダブルタップ対応

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,4 +4,23 @@
   container.addEventListener("dblclick", () => {
     document.location = "/";
   });
+
+  var tapCount = 0 ;
+
+  container.addEventListener("touchstart", (e) => {
+    // シングルタップ判定
+    if( !tapCount ) {
+      ++tapCount ;
+  
+      setTimeout( () => {
+        tapCount = 0 ;
+      }, 350 ) ;
+  
+    // ダブルタップ判定
+    } else {
+      e.preventDefault() ;
+      document.location = "/";
+      tapCount = 0 ;
+    }
+  });
 }
